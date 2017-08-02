53fc45027bce2c456f9a939cab7fce475223ebac
sujay-dsa <30657643+sujay-dsa@users.noreply.github.com>
2017-08-02 20:34:39 +0530

    Update Calculatior.R

--- R/Calculatior.R
+++ R/Calculatior.R
@@ -1,8 +1,9 @@
 
 calculate_score <- function(fileName){
-library(xlsx)
-# param_values <- read.xlsx2("param values for R program.xlsx",sheetName = "Sheet1")
- 
 
-return(86)
+# param_values <- read.xlsx2("param values for R program.xlsx",sheetName = "Sheet1")
+ list(
+    message = paste("hello! This is", R.Version()$version.string)
+  )
+
 }
