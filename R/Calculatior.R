87c90f1b259616e38a98ecb16663193448218672
sujay-dsa <30657643+sujay-dsa@users.noreply.github.com>
2017-08-02 20:38:32 +0530

    Update Calculatior.R

--- R/Calculatior.R
+++ R/Calculatior.R
@@ -2,9 +2,9 @@
 calculate_score <- function(fileName){
 
 # param_values <- read.xlsx2("param values for R program.xlsx",sheetName = "Sheet1")
- 
+ param_weights <- read.csv("G:\\Wisdom Tree\\Scoring\\Web Service\\weights.csv")
  list(
-    message = 86
+    message = param_weights$Category[1]
   )
 
 }
