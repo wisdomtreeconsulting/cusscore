297918431d020efc0add48dc4a8e1ba13e23a3e4
sujay-dsa <30657643+sujay-dsa@users.noreply.github.com>
2017-08-06 23:37:43 +0530

    Add files via upload

--- R/Calculatior.R
+++ R/Calculatior.R
@@ -1,41 +1,63 @@
+calculate_score <- function(date){
 
-calculate_score <- function(fileName){
+# Set the working Directory to source
+# Need to figure out
 
-param_weights <- read.csv("G:\\Wisdom Tree\\Scoring\\Web Service\\weights.csv")
+
+param_weights <- read.csv("G:\\Wisdom Tree\\Scoring\\Web Service\\Data_Tables\\ParamWeightMappings.csv")
+cust_metadata <- read.csv("G:\\Wisdom Tree\\Scoring\\Web Service\\Data_Tables\\CustomerData.csv")
+raw_input <- read.csv("G:\\Wisdom Tree\\Scoring\\Web Service\\Data_Tables\\CustParamMapping.csv")
+
 
 str(param_weights)
-names(param_weights)
-cols = c(4:15)
-param_weights[,cols] =apply(param_weights[,cols], 2, function(x) as.numeric(as.character(x)))
+str(cust_metadata)
+str(raw_input)
+
+combined_inputs <- merge(x = param_weights, y = raw_input, by=c("cust_id","param_id"))
 
 
-compute_Scores <- function(){sapply(param_weights$PARAM.DATA, function(x){
 
-   ifelse(x <= param_weights$Static.Range..penalty.[1],
-          x*param_weights$Static.weight..penalty.*param_weights$Dynamic..weight..penalty.,
-            ifelse(x <= param_weights$Static.Range..Base.[1],
-               x*param_weights$Static.Weight...Base. *param_weights$Dynamic.Weight...Base.,
-                  ifelse(x<=param_weights$Static.Range..Incentive.,x,
-                        x*param_weights$Static.Weight..Incentive.*param_weights$Dynamic..Weight..Incentive.)))
+compute_Scores <- function(){sapply(combined_inputs$value, function(x){
+
+   ifelse(x <= combined_inputs$penalty_range[1],
+          x*combined_inputs$static_penalty_weight*combined_inputs$dynamic_penalty_weight,
+            ifelse(x <= combined_inputs$base_range[1],
+               x*combined_inputs$static_base_weight *combined_inputs$dynamic_base_weight,
+                  ifelse(x<=combined_inputs$incentive_range,x,
+                        x*combined_inputs$static_incentive_weight*combined_inputs$dynamic_incentive_weight)))
 })
 
 }
 
 # Restrict max value to 100
-param_weights$SCORE <- pmin(compute_Scores(),100)
+combined_inputs$SCORE <- pmin(compute_Scores(),100)
 
-final_scores <- aggregate(round(SCORE*Weight)~ Customer.Id+Category, data = param_weights, sum)
-colnames(final_scores) <- c("CustomerID","Category","Score")
+final_scores <- aggregate(round(SCORE*weight)~ cust_id+Category+date, data = combined_inputs, sum)
+
+
+colnames(final_scores) <- c("CustomerID","Category","Date","Score")
 
 #Get to desired Format
 library(reshape2)
-final_scores <- dcast(final_scores, CustomerID~Category,value.var = "Score")
+final_scores <- dcast(final_scores, CustomerID+Date~Category,value.var = "Score")
+final_scores <- merge(final_scores, cust_metadata, by.x =c("CustomerID"),by.y = c("cust_id"))
+
+
 
 final_scores$SUCCESS_SCORES <- 0.1*final_scores$FIT+0.5*final_scores$HEALTH + 0.4*final_scores$VALUE
-library(jsonlite)
-                         
- list(
-    message = toJSON(final_scores)
-  )
 
+library(jsonlite)
+library(mongolite)
+
+customerCollection <- mongo(collection = "customer_score", db="local")
+
+
+customerCollection$insert(final_scores)
+
+final_output <- customerCollection$find('{"Date":"1-Jul-17"}',sort = '{"SUCCESS_SCORES": -1}',limit = 10,
+                        fields = '{"_id":false,"Cust_Name":true,"FIT": true,"HEALTH" : true,
+                        "VALUE": true,"SUCCESS_SCORES":true}')
+
+
+return(fit_score)
 }
