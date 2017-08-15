677045b3e4aeecf4d2527ff59d844206c8d1d9bc
wisdomtreeconsulting <30921918+wisdomtreeconsulting@users.noreply.github.com>
2017-08-15 16:48:24 +0530

    Update Calculatior.R

--- R/Calculatior.R
+++ R/Calculatior.R
@@ -4,7 +4,7 @@ calculate_score <- function(date){
 # Need to figure out
 
 # setwd("G://Wisdom Tree//Scoring//Web Service//MyPackage")
-   setwd("/home/sujay")
+   setwd("/")
 param_weights <- read.csv("../Data_Tables/ParamWeightMappings.csv")
 cust_metadata <- read.csv("../Data_Tables/CustomerData.csv")
 raw_input <- read.csv("../Data_Tables/CustParamMapping.csv")
