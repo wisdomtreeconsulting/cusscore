55cb6e256d66f3a8ba5c52c049e93113ff40b8cd
wisdomtreeconsulting <30921918+wisdomtreeconsulting@users.noreply.github.com>
2017-08-15 17:06:27 +0530

    Update Calculatior.R

--- R/Calculatior.R
+++ R/Calculatior.R
@@ -5,9 +5,9 @@ calculate_score <- function(date){
 
 # setwd("G://Wisdom Tree//Scoring//Web Service//MyPackage")
    setwd("/")
-param_weights <- read.csv("../Data_Tables/ParamWeightMappings.csv")
-cust_metadata <- read.csv("../Data_Tables/CustomerData.csv")
-raw_input <- read.csv("../Data_Tables/CustParamMapping.csv")
+param_weights <- read.csv("/Data_Tables/ParamWeightMappings.csv")
+cust_metadata <- read.csv("/Data_Tables/CustomerData.csv")
+raw_input <- read.csv("/Data_Tables/CustParamMapping.csv")
 
 
 combined_inputs <- merge(x = param_weights, y = raw_input, by=c("cust_id","param_id"))
