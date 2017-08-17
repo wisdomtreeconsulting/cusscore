ca2532810301469ad12c3cb1f4499685f854dba5
wisdomtreeconsulting <30921918+wisdomtreeconsulting@users.noreply.github.com>
2017-08-17 11:34:52 +0530

    Update Calculatior.R

--- R/Calculatior.R
+++ R/Calculatior.R
@@ -2,58 +2,50 @@ calculate_score <- function(date){
 
 # Set the working Directory to source
 # Need to figure out
-library(utils)
  #setwd("G://Wisdom Tree//Scoring//Web Service//MyPackage")
-   setwd("/home/wisdomtree")
- tryCatch({
-param_weights <- read.csv("/home/wisdomtree/ParamWeightMappings.csv")
-cust_metadata <- read.csv("/home/wisdomtree/Data_Tables/CustomerData.csv")
-raw_input <- read.csv("/home/wisdomtree/Data_Tables/CustParamMapping.csv")
- }, warning = function(w) {
+#   setwd("/home/wisdomtree")
+# tryCatch({
+#param_weights <- read.csv("/home/wisdomtree/ParamWeightMappings.csv")
+#cust_metadata <- read.csv("/home/wisdomtree/Data_Tables/CustomerData.csv")
+#raw_input <- read.csv("/home/wisdomtree/Data_Tables/CustParamMapping.csv")
+# }, warning = function(w) {
 
-}, error = function(e) {
-write(e, "errortxt.txt")
-}, finally = {
+#}, error = function(e) {
+#write(e, "errortxt.txt")
+#}, finally = {
 
-})
+#})
    
 # param_weights <- read.csv("../Data_Tables/ParamWeightMappings.csv")
 #cust_metadata <- read.csv("../Data_Tables/CustomerData.csv")
 #raw_input <- read.csv("../Data_Tables/CustParamMapping.csv")
 
-combined_inputs <- merge(x = param_weights, y = raw_input, by=c("cust_id","param_id"))
+#combined_inputs <- merge(x = param_weights, y = raw_input, by=c("cust_id","param_id"))
+#compute_Scores <- function(){sapply(combined_inputs$value, function(x){
+#   ifelse(x <= combined_inputs$penalty_range[1],
+#          x*combined_inputs$static_penalty_weight*combined_inputs$dynamic_penalty_weight,
+#            ifelse(x <= combined_inputs$base_range[1],
+#               x*combined_inputs$static_base_weight *combined_inputs$dynamic_base_weight,
+#                 ifelse(x<=combined_inputs$incentive_range,x,
+#                        x*combined_inputs$static_incentive_weight*combined_inputs$dynamic_incentive_weight)))
+#})
 
-
-
-
-compute_Scores <- function(){sapply(combined_inputs$value, function(x){
-
-   ifelse(x <= combined_inputs$penalty_range[1],
-          x*combined_inputs$static_penalty_weight*combined_inputs$dynamic_penalty_weight,
-            ifelse(x <= combined_inputs$base_range[1],
-               x*combined_inputs$static_base_weight *combined_inputs$dynamic_base_weight,
-                  ifelse(x<=combined_inputs$incentive_range,x,
-                        x*combined_inputs$static_incentive_weight*combined_inputs$dynamic_incentive_weight)))
-})
-
-}
+#}
 
 # Restrict max value to 100
-combined_inputs$SCORE <- pmin(compute_Scores(),100)
+#combined_inputs$SCORE <- pmin(compute_Scores(),100)
 
-final_scores <- aggregate(round(SCORE*weight)~ cust_id+Category+date, data = combined_inputs, sum)
+#final_scores <- aggregate(round(SCORE*weight)~ cust_id+Category+date, data = combined_inputs, sum)
 
 
-colnames(final_scores) <- c("CustomerID","Category","Date","Score")
+#colnames(final_scores) <- c("CustomerID","Category","Date","Score")
 
 #Get to desired Format
-library(reshape2)
-final_scores <- dcast(final_scores, CustomerID+Date~Category,value.var = "Score")
-final_scores <- merge(final_scores, cust_metadata, by.x =c("CustomerID"),by.y = c("cust_id"))
+#library(reshape2)
+#final_scores <- dcast(final_scores, CustomerID+Date~Category,value.var = "Score")
+#final_scores <- merge(final_scores, cust_metadata, by.x =c("CustomerID"),by.y = c("cust_id"))
 
-
-
-final_scores$SUCCESS_SCORES <- round(0.1*final_scores$FIT+0.5*final_scores$HEALTH + 0.4*final_scores$VALUE)
+#final_scores$SUCCESS_SCORES <- round(0.1*final_scores$FIT+0.5*final_scores$HEALTH + 0.4*final_scores$VALUE)
 
 library(jsonlite)
 library(mongolite)
@@ -61,7 +53,7 @@ library(mongolite)
 customerCollection <- mongo(collection = "cusscore", db="local")
 
 tryCatch({
-  customerCollection$insert(final_scores)
+#  customerCollection$insert(final_scores)
 
   }, warning = function(w) {
 
@@ -71,7 +63,7 @@ tryCatch({
 
 })
 
-    d <- as.Date( as.character(date), "%m/%d/%Y")
+   d <- as.Date( as.character(date), "%m/%d/%Y")
    date <-  format(d, "%d/%m/%Y")
 
 final_output <- customerCollection$find(paste('{"Date":"',as.character(date),'"}',sep=""),sort = '{"SUCCESS_SCORES": -1}',limit = 10,
