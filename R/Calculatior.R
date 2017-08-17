20dea104df4d9002f2aacaae433afd13e4f314e7
wisdomtreeconsulting <30921918+wisdomtreeconsulting@users.noreply.github.com>
2017-08-17 16:33:09 +0530

    Update Calculatior.R

--- R/Calculatior.R
+++ R/Calculatior.R
@@ -63,15 +63,14 @@ tryCatch({
 }, finally = {
 
 })
-date <- "07/01/2017"
+
    d <- as.Date( as.character(date), "%m/%d/%Y")
    date <-  format(d, "%d/%m/%Y")
 
 final_output <- customerCollection$find(paste('{"Date":"',as.character(date),'"}',sep=""),sort = '{"SUCCESS_SCORES": -1}',limit = 10,
                                         fields = '{"_id":false,"Cust_Name":true,"FIT": true,"HEALTH" : true,
                                         "VALUE": true,"SUCCESS_SCORES":true,"CustomerID":true}')
-print(final_output)
- print(nrow(final_output))
+
 #need to find last 7 days success scores for the customer ids
 trend_customers <- final_output$CustomerID
 
@@ -93,7 +92,6 @@ trend_scores <- customerCollection$aggregate(paste('[
 
 ]'))
 
-write.csv(trend_scores,file="trendscores.csv")
  
 trend_scores <- cbind(trend_scores[,1],trend_scores[,2])
 colnames(trend_scores) <- c("CustomerID","id","Date","Trend_Scores")
